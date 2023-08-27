b298c8f20ac32673dc682f5a5e9dad6cc76d971b
Josseth Arroyo <55711477+cheche482010@users.noreply.github.com>
2023-08-27 19:20:34 -0400

    responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,11 @@ const DropdownMenu = ({ title, options }) => {
   };
 
   return (
-    <div className="dropdown" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <div
+      className={`dropdown ${isOpen ? 'open' : ''}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <span>{title}</span><i className="fa-solid fa-chevron-down"></i>
       {isOpen && (
         <div className="dropdown-content">
@@ -36,6 +40,11 @@ const Button = ({ className, iconClass, label }) => (
 
 const Header = () => {
   const itemOptions = ['Option 1', 'Option 2'];
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <div className="App">
@@ -44,17 +53,25 @@ const Header = () => {
           <img alt="Logo Chick" src={logoChick} />
         </div>
 
-        <DropdownMenu title="CURRENCY" options={['Option 1', 'Option 2']} />
-        <DropdownMenu title="ITEM" options={itemOptions} />
-        <DropdownMenu title="ACCOUNT" options={['Option 1', 'Option 2']} />
-        <DropdownMenu title="SERVICES" options={['Option 1', 'Option 2']} />
-        <DropdownMenu title="SWAPS" options={['Option 1', 'Option 2']} />
-        <DropdownMenu title="SELL" options={['Option 1', 'Option 2']} />
+        <div className={`menu ${menuOpen ? 'open' : ''}`}>
+          <DropdownMenu title="CURRENCY" options={['Option 1', 'Option 2']} />
+          <DropdownMenu title="ITEM" options={itemOptions} />
+          <DropdownMenu title="ACCOUNT" options={['Option 1', 'Option 2']} />
+          <DropdownMenu title="SERVICES" options={['Option 1', 'Option 2']} />
+          <DropdownMenu title="SWAPS" options={['Option 1', 'Option 2']} />
+          <DropdownMenu title="SELL" options={['Option 1', 'Option 2']} />
+        </div>
 
-        <div className="buttons">
+        <button className="menu-toggle" onClick={handleMenuToggle}>
+          <i className={`fa-solid ${menuOpen ? 'fa-times' : 'fa-bars'}`}></i>
+        </button>
+
+        <div className={`right-elements ${menuOpen ? 'open' : ''}`}>
           <DropdownMenu title="USD" options={['USD', 'EUR']} />
-          <Button className="buy-btn" iconClass="fas fa-shopping-cart" label="Cart (5)" />
-          <Button className="sign-in-btn" iconClass="fas fa-sign-in-alt" label="Sign In" />
+          <div className="buttons">
+            <Button className="buy-btn" iconClass="fas fa-shopping-cart" label="Cart (5)" />
+            <Button className="sign-in-btn" iconClass="fas fa-sign-in-alt" label="Sign In" />
+          </div>
         </div>
       </nav>
     </div>
